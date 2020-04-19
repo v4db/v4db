46fcc1f42204a4d94bceabbb7ce5b292fe09f373
v4db <paniom.tinlok@gmail.com>
2020-04-19 21:21:24 +0800

    initial db changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 // const path = require('path')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // express()
 //   .use(express.static(path.join(__dirname, 'public')))
@@ -10,13 +10,57 @@ const PORT = process.env.PORT || 3000
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const app = express();
+const bodyParser = require(`body-parser`);
 const Database = require(`./db`);
 
+const jsonParser = bodyParser.json();
+
 app.get(`/`, async (req, res) => {
-    const collection = Database.connection.then(client => client.db(`creature`)).then(db => db.collection(`illustration`));
-    const array = await collection.then(collection => collection.find().toArray());
-    res.json(array);
+    const continentCollection = Database.connection.then(client => client.db(`world`)).then(db => db.collection(`continent`));
+    const creatureCollection = Database.connection.then(client => client.db(`creature`)).then(db => db.collection(`illustration`));
+    let continents = await continentCollection.then(collection => collection.find().toArray());
+    const illustrations = await creatureCollection.then(collection => collection.find().toArray());
+    continents.forEach(continent => {
+        continent.regions.forEach(region => {
+            region.places.forEach(place => {
+                place.creatures = place.creatures.map(creatureId => {
+                    return illustrations.find(illustration => {
+                        console.log(`${illustration._id} ${creatureId} ${illustration._id == creatureId}`)
+                        return illustration._id == creatureId;
+                    });
+                });
+            });
+        });
+    });
+    res.json(continents);
 });
-// app.post(`/creatures/{id}`)
+
+app.post(`/creatures/:id`, jsonParser, async (req, res) => {
+    const creatureCollection = Database.connection.then(client => client.db(`creature`)).then(db => db.collection(`illustration`));
+    await creatureCollection.then(collection => collection.updateOne(
+        { 
+            _id: new Database.ObjectID(req.params.id)
+        },
+        {
+            $set: req.body
+        }
+    ));
+    const continentCollection = Database.connection.then(client => client.db(`world`)).then(db => db.collection(`continent`));
+    let continents = await continentCollection.then(collection => collection.find().toArray());
+    const illustrations = await creatureCollection.then(collection => collection.find().toArray());
+    continents.forEach(continent => {
+        continent.regions.forEach(region => {
+            region.places.forEach(place => {
+                place.creatures = place.creatures.map(creatureId => {
+                    return illustrations.find(illustration => {
+                        console.log(`${illustration._id} ${creatureId} ${illustration._id == creatureId}`)
+                        return illustration._id == creatureId;
+                    });
+                });
+            });
+        });
+    });
+    res.json(continents);
+});
 
 app.listen(PORT);
