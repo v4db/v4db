a6f47c202a1a785a47722efaa93d82d95b97c547
v4db <paniom.tinlok@gmail.com>
2020-04-19 18:53:40 +0800

    initial db changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 // const path = require('path')
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 // express()
 //   .use(express.static(path.join(__dirname, 'public')))
@@ -19,4 +19,4 @@ app.get(`/`, async (req, res) => {
 });
 // app.post(`/creatures/{id}`)
 
-app.listen(3000);
+app.listen(PORT);
